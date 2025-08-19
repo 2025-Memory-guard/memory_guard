42d711812136e88423c382f32e57cbd3d07e07a6
junyeong <whwnsdud4592@naver.com>
2025-08-19 10:22:41 +0900

    refactor: User엔티티 서영님이 수정하기 전으로 변경

---
+++ src/main/java/com/example/memory_guard/user/dto/ExistsResponseDto.java
@@ -0,0 +1,10 @@
+package com.example.memory_guard.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ExistsResponseDto {
+  private boolean exists;
+}
